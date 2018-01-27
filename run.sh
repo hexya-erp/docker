1c948e508047c190f73e0d3156477c4d1832df68
Nicolas Piganeau <nicolas.piganeau@ndp-systemes.fr>
2018-01-27 18:20:57 +0100

    Switched to golang 1.10-rc image

--- run.sh
+++ run.sh
@@ -5,13 +5,9 @@ echo "1 - Generating Hexya..."
 hexya generate 2>/dev/null
 
 echo ""
-echo "2 - Compiling Hexya..."
-cd $GOPATH/src/github.com/hexya-erp/hexya && go install ./...
-
-echo ""
-echo "3 - Updating db..."
+echo "2 - Updating db..."
 hexya updatedb
 
 echo ""
-echo "4 - Starting server..."
+echo "3 - Starting server..."
 hexya server
